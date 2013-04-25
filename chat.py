c44c2f084b215870334ea20e0b777571e2f19ac9
Eric Owen <ezowen@vt.edu>
2013-04-25 13:22:11 -0400

    adds entry to convo upon press of enter key

--- chat.py
+++ chat.py
@@ -3,7 +3,37 @@ from Tkinter import*
 root = Tk()
 root.title("Chat")
 
-fContacts = Frame(root, width=100, height=300)
-fContacts.pack(side=LEFT)
+#event to send message and clear entry field
+def send(event):
+	convo.config(state=NORMAL)
+	convo.insert(END, entry.get()) #add entry to convo
+	convo.insert(END, '\n')
+	convo.config(state=DISABLED)
+	entry.delete(0, END) #clear entry
+
+
+#menubar stuff
+menubar= Menu(root)
+menubar.add_command(label="file")
+menubar.add_command(label="contacts")
+menubar.add_command(label="conversation")
+root.config(menu=menubar)
+
+#left frame to hold list of contacts
+leftframe = Frame(root, width=100)
+leftframe.pack(side=LEFT)
+
+#right frame to hold conversation and input
+rightframe = Frame(root)
+convo = Text(rightframe, state=DISABLED, width=50, height=15)
+convo.pack(side=TOP)
+entry = Entry(rightframe, width=40)
+entry.pack(side=BOTTOM)
+entry.bind("<Return>", send)
+rightframe.pack()
+
+
+
+
 
 root.mainloop()
