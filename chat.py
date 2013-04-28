b2ff900e56549e8de8fdd6082fded64e75c3d16d
Eric <ezo@ezo-H67A-UD3H-B3.(none)>
2013-04-28 12:15:47 -0400

    added functions to host, no testing done

--- chat.py
+++ chat.py
@@ -4,33 +4,17 @@ import socket
 contactsName = [] #create empty list for contact names
 contactsInfo = [] #create empty list for contacts connection info
 
-#client thingy
-def thing():
-	s = socket.socket()
-	host = socket.gethostname()
-	port = 2328
-	s.connect((host, port))
-	print s.recv(1024)
+s = socket.socket()
+host = socket.gethostname()
+port = 2328
+s.connect((host, port))
+print s.recv(1024)
 
-#host thingty
-def thingy():
-	s = socket.socket()
-	host = socket.gethostname()
-	port = 2328
-	s.bind((host, port))
-	s.listen(1)
-	addr = ""
-	c = ""
-	while (addr==""):
-		c, addr = s.accept()
-		print "Host got a connection woo"
-		c.send("message sent to client")
 		
 #sends request to start a new conversation
 def sendRequest(contactinfo):
 	global conactsinfo
-	
-		
+
 #set up a new connection/contact/converation
 def newconvo():
 	startnew = Toplevel()
@@ -49,8 +33,6 @@ def newconvo():
 	newframe.pack()
 	startnew.mainloop()
 	
-	
-
 #event to send message and clear entry field
 def send(event):
 	convo.config(state=NORMAL)
@@ -63,40 +45,68 @@ def send(event):
 def checkWhosOnline():
 	global conactsInfo
 
+#sends user info to host to go online
+def login():
+	global contactsInfo
 
-#start GUI
+def newUser(user, passw):
+	file = open('userInfo.txt', 'w+')
+	
+
+#window for user to login, stores data in userInfo.txt
+def loginPrompt():
+	login = Toplevel()
+	login.title ("Log in")
+	loginframe = Frame(login, padx=10, pady=10)
+	username = Label (loginframe, text="Username:", pady=5)
+	username.grid (row=0, column=0)
+	user = Entry (loginframe, width=20)
+	user.grid (row=0, column=1)
+	password = Label (loginframe, text="Password:", pady=5)
+	password.grid (row=1, column=0)
+	passw = Entry (loginframe, width=20)
+	passw.grid (row=1, column=1)
+	logbutton = Button (loginframe, text="Log in", command=login)
+	logbutton.grid(row=3, column=0)
+	newbutton = Button (loginframe, text="New user")
+	newbutton.grid(row=3, column=1)
+	loginframe.pack()
+	login.mainloop()
+
+
+#----------------------------------------------------END OF FUNCTIONS-------------------------------------------
+
+
+
+
+
+#socket set up stuff
+
+#STARTING GUI
 root = Tk()
-root.title("tiddly winks")
-
+root.title("Main window")
 #menubar stuff
 menubar= Menu(root)
-
 file = Menu(menubar, tearoff=0)
+file.add_command(label="Log in", command=loginPrompt)
 file.add_command(label="New", command=newconvo)
 file.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=file)
-
 root.config(menu=menubar)
-
 #left frame to hold list of contacts
 leftframe = Frame(root, width=100)
 leftframe.pack(side=LEFT)
-
 #right frame to hold conversation and input
 rightframe = Frame(root)
-
 entry = Entry(rightframe, width=40)
 entry.pack(side=BOTTOM, fill=X)
 entry.bind("<Return>", send)
-
 convo = Text(rightframe, state=DISABLED, width=50, height=15)
 convo.pack(side=LEFT, fill=X, expand=1)
-
 scroll = Scrollbar(rightframe, command=convo.yview)
 scroll.pack(side=LEFT, fill=Y, expand=1)
 convo.config(yscrollcommand=scroll.set)
-
 rightframe.pack(side=RIGHT, fill=BOTH, expand=1)
-
 root.mainloop()
+
 
--- host.py
+++ host.py
@@ -1,18 +1,69 @@
+#===== TO DOs ======
+#function to change status to online
+#funciton to send client list of whos online
+#make sure client doesnt sent more than 1024 bytes to host
+
+
+
 from Tkinter import*
 import socket
+import threading
 
+#if user is new, add them to the file
+def newUser(user, passw):
+	file = open('hostUserInfo.txt', 'w+')
+	f.write(user . '\n' . passw . '\n')
+	f.close
 
+#check if user exists
+def userExists(userlist):
+	found = 0
+	for line in userlist:
+		if 
+
+#authenticate user and change online status
+def authenticate(user, passw):
+	if user in open('hostUserInfo.txt').read(): #if user is already in file
+		#authenticate
+		#log them in
+	else:
+		#add them to file
+		#log them in
+	
+
+#create a thread to handle recieved client data
+#should take data, send data, and close connection
+def clientThread(conn):
+	open = 1
+	while open==1:
+		dest = conn.recv(1024)
+		data = conn.recv(1024)
+		
+		if dest=="login":
+			passw = conn.recv(1024)
+			authenticate(data, passw)
+		else:
+			
+
+#socket stuff
+s = socket.socket()
+host = socket.gethostname()
+port = 22222
+s.bind((host, port))
+s.listen(5)
+
+while True:
+	conn, addr = s.accept()
+	start_new_thread(clientThread, (conn,))
 	
 
 #GUI stuff
 root = Tk()
 root.title("hostess cakes")
-
 activity = Text (root, width=50)
 activity.insert(END, "host is running..\n")
 activity.config(state=DISABLED)
 activity.pack()
-
+closesock = Button(root, text="close socket", command=s.close())
 root.mainloop()
-
 
